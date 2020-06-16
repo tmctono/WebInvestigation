fdd1ddbc4de2897b30ccefee3db527f5c8c29388
Manabu Tonosaki <manabu@tomarika.com>
2020-06-16 11:14:23 +0900

    Add SqlServer Controller

---
+++ WebInvestigation/Controllers/SqlServerController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tono;
+using WebInvestigation.Models;
+
+namespace WebInvestigation.Controllers
+{
+    public class SqlServerController : Controller
+    {
+        private string GetCookie(string key, string def = "")
+        {
+            return Request.Cookies[key] ?? def;
+        }
+        private void SetCookie(string key, string value)
+        {
+            Response.Cookies.Append(key, value, new CookieOptions
+            {
+                Secure = true,
+                HttpOnly = true,
+                SameSite = SameSiteMode.Strict,
+                Expires = DateTimeOffset.UtcNow + TimeSpan.FromDays(14),
+            });
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Index(new SqlServerModel
+            {
+                ConnectionString = SqlServerModel.Default.ConnectionString,
+                Sql = SqlServerModel.Default.Sql,
+                SkipSql = true,
+            });
+        }
+
+        [RequireHttps]
+        [HttpPost]
+        public IActionResult Index(SqlServerModel model)
+        {
+            // Apply input history from cookie
+            var sc = GetCookie("SqlServerModel_ConnectionString", SqlServerModel.Default.ConnectionString);
+            if (string.IsNullOrEmpty(model.ConnectionString) || model.ConnectionString.Equals(SqlServerModel.Default.ConnectionString))
+            {
+                model.ConnectionString = sc;
+            }
+            else
+            {
+                SetCookie("SqlServerModel_ConnectionString", model.ConnectionString);
+            }
+            var sq = GetCookie("SqlServerModel_Sql", SqlServerModel.Default.Sql);
+            if (string.IsNullOrEmpty(model.Sql.Trim()) || model.Sql.Replace(" ", "").Equals(SqlServerModel.Default.Sql.Replace(" ", "")))
+            {
+                model.Sql = sq;
+            }
+            else
+            {
+                SetCookie("SqlServerModel_Sql", model.Sql);
+            }
+            if (!model.SkipSql)
+            {
+                try
+                {
+                    var sql = model.Sql.Trim();
+                    if (Regex.IsMatch(sql.ToUpper(), "DROP\\s+TABLE\\s+"))
+                    {
+                        throw new NotSupportedException("Cannot SQL contains drop table command.");
+                    }
+                    if (Regex.IsMatch(sql.ToUpper(), "DELETE\\s+FROM\\s+"))
+                    {
+                        throw new NotSupportedException("Cannot SQL contains delete command.");
+                    }
+                    if (Regex.IsMatch(sql.ToUpper(), "UPDATE\\s+.+SET\\s.+"))
+                    {
+                        throw new NotSupportedException("Cannot SQL contains update command.");
+                    }
+                    if (Regex.IsMatch(sql.ToUpper(), "INSERT\\s+INTO\\s+"))
+                    {
+                        throw new NotSupportedException("Cannot SQL contains insert command.");
+                    }
+
+                    if (sql.StartsWith("SELECT ", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        // remove input TOP(*) word
+                        var mstr = Regex.Match(sql, "\\s+[Tt][Oo][Pp]\\s*\\(\\d+\\)\\s+");
+                        if(mstr.Success)
+                        {
+                            sql = sql.Replace(mstr.Value, " ");
+                        }
+                        sql = "SELECT TOP(20) " + StrUtil.MidSkip(sql, "SELECT\\s+");
+                    }
+                    model.LatestSql = sql;
+
+                    using var con = new SqlConnection(model.ConnectionString);
+                    con.Open();
+                    using var sqlcommand = new SqlCommand(sql, con);
+                    using var row = sqlcommand.ExecuteReader();
+
+                    var cs = row.GetColumnSchema();
+                    model.Table = new List<List<string>>();
+                    model.Table.Add(cs.Select(a => a.ColumnName).ToList());
+                    model.Table.Add(cs.Select(a => $"<{a.DataTypeName}>").ToList());
+
+                    while (row.Read())
+                    {
+                        var r = new List<string>();
+                        for (var col = 0; col < row.FieldCount; col++)
+                        {
+                            r.Add(Tono.DbUtil.ToString(row[col], "(null)"));
+                        }
+                        model.Table.Add(r);
+                        if (model.Table.Count > 22)
+                        {
+                            break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    model.ErrorMessage = ex.Message;
+                }
+            }
+
+            if (model.Table == null)
+            {
+                model.Table = new List<List<string>>();
+            }
+            while (model.Table.Count < 2)
+            {
+                model.Table.Add(new List<string>());
+            }
+
+            model.SkipSql = false;
+            return View(model);
+        }
+    }
+}
---
+++ WebInvestigation/Models/SqlServerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebInvestigation.Models
+{
+    public class SqlServerModel
+    {
+        public static readonly SqlServerModel Default = new SqlServerModel
+        {
+            ConnectionString = "Server=tcp:<your sqlserver here>.database.windows.net,1433;Initial Catalog=<your database name here>;Persist Security Info=False;User ID=<your sql server account here>;Password=<the password here>;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;",
+            Sql = "SELECT * FROM sys.objects WHERE type = 'U';",
+        };
+        public bool SkipSql { get; set; }
+        public string ConnectionString { get; set; }
+        public string Sql { get; set; }
+        public string LatestSql { get; set; }
+        public string ErrorMessage { get; set; }
+        public List<List<string>> Table { get; set; }
+    }
+}
