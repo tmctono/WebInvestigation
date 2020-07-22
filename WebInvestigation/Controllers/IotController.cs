7aec6e827a24e7e4c143ceecd3cc9faa9792436e
Manabu Tonosaki <tmctono@aqtono.com>
2020-07-22 10:07:15 +0900

    IoT Base Page

---
+++ WebInvestigation/Controllers/IotController.cs
@@ -0,0 +1,38 @@
+﻿// (c) 2020 Manabu Tonosaki
+// Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TonoAspNetCore;
+using WebInvestigation.Models;
+
+namespace WebInvestigation.Controllers
+{
+    [RequireHttps]
+    public class IotController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new IotModel
+            {
+                DeviceId = IotModel.Default.DeviceId,
+                DeviceKey = IotModel.Default.DeviceKey,
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Index(IotModel model)
+        {
+            var cu = ControllerUtils.From(this);
+            cu.PersistInput("DeviceId", model, IotModel.Default.DeviceId);
+            cu.PersistInput("DeviceKey", model, IotModel.Default.DeviceKey);
+
+            return View(model);
+        }
+    }
+}
+
---
+++ WebInvestigation/Models/IotModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebInvestigation.Models
+{
+    public class IotModel
+    {
+        public static readonly IotModel Default = new IotModel
+        {
+            DeviceId = "<your device id>",
+            DeviceKey = "<your device shared key>",
+        };
+        public string DeviceId { get; set; }
+        public string DeviceKey { get; set; }
+        public string Result { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
