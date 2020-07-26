bd8fcfb9d1681fe91599b2070ec5f2e76e6eb212
Manabu Tonosaki <manabu@tomarika.com>
2020-07-26 20:45:55 +0900

    IoT Hub (Send)

--- WebInvestigation/Controllers/IotController.cs
+++ WebInvestigation/Controllers/IotController.cs
@@ -4,8 +4,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using DotNetty.Common.Utilities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Devices.Client;
 using TonoAspNetCore;
 using WebInvestigation.Models;
 
@@ -17,10 +21,12 @@ namespace WebInvestigation.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return View(new IotModel
+            return Index(new IotModel
             {
+                HostName = IotModel.Default.HostName,
                 DeviceId = IotModel.Default.DeviceId,
                 DeviceKey = IotModel.Default.DeviceKey,
+                Message = $"IoT Test Message at ",
             });
         }
 
@@ -28,8 +34,31 @@ namespace WebInvestigation.Controllers
         public IActionResult Index(IotModel model)
         {
             var cu = ControllerUtils.From(this);
+            cu.PersistInput("HostName", model, IotModel.Default.HostName);
             cu.PersistInput("DeviceId", model, IotModel.Default.DeviceId);
             cu.PersistInput("DeviceKey", model, IotModel.Default.DeviceKey);
+
+            if (model.Message.StartsWith("IoT Test Message at "))
+            {
+                model.Message = $"IoT Test Message at {DateTime.UtcNow.ToString(Tono.TimeUtil.FormatYMDHMS)}";
+            }
+
+            // Send Message to IoT Hub
+            try
+            {
+                var client = DeviceClient.Create(model.HostName, new DeviceAuthenticationWithRegistrySymmetricKey(model.DeviceId, model.DeviceKey), model.TransportType);
+                var mes = new Message(Encoding.UTF8.GetBytes(model.Message));
+                client.SendEventAsync(
+                    message: mes,
+                    cancellationToken: new CancellationTokenSource(TimeSpan.FromMilliseconds(5000)).Token
+                ).ConfigureAwait(false).GetAwaiter().GetResult();
+
+                model.Result = $"Successfully sent the message '{model.Message}' from Device [{model.DeviceId}]";
+            }
+            catch (Exception ex)
+            {
+                model.ErrorMessage = ex.Message;
+            }
 
             return View(model);
         }
--- WebInvestigation/Models/IotModel.cs
+++ WebInvestigation/Models/IotModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FastEnumUtility;
+using Microsoft.Azure.Devices.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,12 +11,18 @@ namespace WebInvestigation.Models
     {
         public static readonly IotModel Default = new IotModel
         {
+            HostName = "<your IoT Hub URI>",
             DeviceId = "<your device id>",
             DeviceKey = "<your device shared key>",
         };
+        public string HostName { get; set; }
         public string DeviceId { get; set; }
         public string DeviceKey { get; set; }
+        public string Message { get; set; }
+        public TransportType TransportType { get; set; }
         public string Result { get; set; }
         public string ErrorMessage { get; set; }
+
+        public IEnumerable<string> GetTransportTypeList() => FastEnum.GetNames<TransportType>();
     }
 }
