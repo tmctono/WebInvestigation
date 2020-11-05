9a9de2d10547ba8fadc296377b6e3be029c8e44d
Manabu Tonosaki <tmctono@aqtono.com>
2020-11-05 14:55:09 +0900

    add some fail tests

--- WebInvestigation/Controllers/HomeController.cs
+++ WebInvestigation/Controllers/HomeController.cs
@@ -1,10 +1,15 @@
 ﻿// (c) 2020 Manabu Tonosaki
 // Licensed under the MIT license.
 
+using DotNetty.Common.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Diagnostics;
 using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using WebInvestigation.Models;
 
 namespace WebInvestigation.Controllers
@@ -34,6 +39,44 @@ namespace WebInvestigation.Controllers
                 model.Set(res);
             }
             return View(model);
+        }
+
+        public IActionResult HttpStatus(HttpStatus status)
+        {
+            return StatusCode(status?.Code ?? 200);
+        }
+
+        public IActionResult ThrowException()
+        {
+            throw new System.Exception($"This is a TEST Exception thrown by user at {DateTime.UtcNow}");
+        }
+
+        public IActionResult StackOverFlow()
+        {
+            StackOverFlow();
+            return View();
+        }
+
+        public IActionResult Cpu()
+        {
+            var cts = new CancellationTokenSource(10000);
+            for (var ii = 0; !cts.IsCancellationRequested; ii++)
+            {
+                Task.Run(() =>
+                {
+                    var rnd = new Random();
+                    var a = "";
+                    for( var i = 0; !cts.IsCancellationRequested; i++)
+                    {
+                        a += ((char)(rnd.NextDouble() * 25 + 'A'));
+                        if (a.Length > 26)
+                        {
+                            a = a.Substring(1, a.Length - 1);
+                        }
+                    }
+                });
+            }
+            return View();
         }
 
         public IActionResult Dns(DnsModel model = null)
